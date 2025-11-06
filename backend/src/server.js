8d983b0863c9aaec9370800f43cda1b5b218b2dc
unknown <dakshgaur369@gmail.com>
2025-11-06 13:15:19 +0530

    initial commit

---
+++ backend/src/server.js
@@ -0,0 +1,10 @@
+import express from "express"
+import { ENV } from "./lib/env.js";
+const app = express()
+console.log(`${ENV.PORT}`);
+app.get("/",(req,res)=>{
+    res.status(200).json({msg:"Success from api 5000"});
+});
+app.listen(ENV.PORT,()=>
+    {(console.log(`Server is running on port ${ENV.PORT}`))}
+)
