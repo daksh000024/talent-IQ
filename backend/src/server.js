d09ebf373eace503d3fdac864b1146540fee8e7e
unknown <dakshgaur369@gmail.com>
2025-11-15 17:31:56 +0530

    Set up database configuration

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,21 @@
 import express from "express"
 import { ENV } from "./lib/env.js";
+import { connectDB } from "./lib/db.js";
+
 const app = express()
-console.log(`${ENV.PORT}`);
+
 app.get("/",(req,res)=>{
     res.status(200).json({msg:"Success from api 5000"});
 });
-app.listen(ENV.PORT,()=>
-    {(console.log(`Server is running on port ${ENV.PORT}`))}
-)
+
+const startServer = async ()=>{
+ try {
+    await connectDB();
+    app.listen(ENV.PORT,()=>{
+    (console.log(`Server is running on port ${ENV.PORT}`))})
+ } catch (error) {
+     console.log("Error starting the server",error)   
+    }
+}
+
+startServer();
